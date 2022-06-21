50ccb83d6b971eaecab6990cc478c300ef7dd7f3
MuzammilHussain <muzammilchuhan.lb@gmail.com>
2022-06-21 14:46:16 +0500

    Update settings.py

--- beauty/beauty/settings.py
+++ beauty/beauty/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!d66(5_^le68ml)9b+1zgztcv&bbl*bdx^wk+&!e33j!8x(0bt'
+SECRET_KEY = 'SECRET API KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
